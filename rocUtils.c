9bf146078f6cecf073e8ec3f1202baf1acc0de04
Bryan Moffit <moffit@jlab.org>
2021-10-14 05:50:42 -0400

    Update description of routines to Bank, instead of user event.
    - add routine to push char* to a bank

--- rocUtils.c
+++ rocUtils.c
@@ -9,13 +9,12 @@
 #include <stdio.h>
 #include <string.h>
 
-/* Routine to read in a file (as text) and create a User Event in a buffer
-
-   Maybe you can pass the rol->dabufp pointer.  that'd be great.
+/* Routine to read in a file (as text) and create a String Bank
+   (uchar*) in a buffer
 */
 
 int
-rocFile2Event(const char *fname, uint8_t *buf,
+rocFile2Bank(const char *fname, uint8_t *buf,
 	      uint16_t banktag, uint8_t banknum,
 	      int32_t maxbytes)
 {
@@ -23,20 +22,11 @@ rocFile2Event(const char *fname, uint8_t *buf,
   int ii, c, ilen, rem;
   FILE *fid;
   int maxb = 1024 * 1024 * 4;	/* max output buffer size */
-  uint32_t ev_header[2];
-  uint32_t *u32p;
+  uint32_t bank_header[2];
 
   if(fname == NULL)
     {
       printf("%s: ERROR: No filename was specified\n", __func__);
-      return -1;
-    }
-
-
-  if((banktag == 0) || (banktag >= 0xff00))
-    {
-      printf("%s: ERROR: Invalid User Bank tag specified (0x%04x)\n",
-	     __func__, banktag);
       return -1;
     }
 
@@ -52,13 +42,13 @@ rocFile2Event(const char *fname, uint8_t *buf,
     }
   else
     {
-      printf("%s: INFO: Opened file %s for reading into User Event \n",
-	     __func__, fname);
+      printf("%s: INFO: Opened file %s for reading into Bank (%x,%x) \n",
+	     __func__, fname, banktag, banknum);
     }
 
   /* Read in the file to the buffer */
   ilen = 8;			/* leave the header words */
-  u32p = (uint32_t *)&buf[0]; /* pointer to beginng of buffer */
+
   while((ilen < maxbytes) && ((c = getc(fid)) != EOF))
     {
       buf[ilen++] = c;
@@ -66,7 +56,8 @@ rocFile2Event(const char *fname, uint8_t *buf,
   fclose(fid);
   printf("%s: INFO: Read %d bytes into buffer\n", __func__, ilen);
 
-  /* Make sure we null terminate the buffer and pad it out to an integal number of words */
+  /* Make sure we null terminate the buffer and pad it out to an
+     integal number of words */
   rem = 4 - (ilen & 0x3);
   if(rem < 4)
     {
@@ -83,20 +74,16 @@ rocFile2Event(const char *fname, uint8_t *buf,
 
   /* Write the header info in the buffer */
   /* Bank Tag */
-  ev_header[0] = (ilen>>2) - 1; /* divide by 4  and subtract 1 */
-  ev_header[1] = (banktag<<16)|(rem<<14)|(3<<8)| banknum;
-  memcpy(&buf[0],&ev_header[0],8);
-  /* u32p[0] =  (ilen >> 2) - 1;	/\* divide by 4  and subtract 1 *\/ */
-  /* u32p[1] = (banktag << 16) | (rem << 14) | (3 << 8) | 0; */
-  /* printf("%s: INFO: Copied Event Header = 0x%08x 0x%08x \n", */
-  /* 	 __func__, ev_header[0], ev_header[1]); */
+  bank_header[0] = (ilen>>2) - 1; /* divide by 4  and subtract 1 */
+  bank_header[1] = (banktag<<16)|(rem<<14)|(3<<8)| banknum;
+  memcpy(&buf[0],&bank_header[0],8);
 
   /*Swap the bytes going to the Async Event 32 bit Fifo */
 #ifdef BYTESWAPIT
   if(0)
     {
       uint8_t aa, bb;
-      for(ii = 8; ii <= (ev_header[0] << 2); ii += 4)
+      for(ii = 8; ii <= (bank_header[0] << 2); ii += 4)
 	{
 	  aa = buf[ii];
 	  bb = buf[ii + 1];
@@ -108,12 +95,82 @@ rocFile2Event(const char *fname, uint8_t *buf,
     }
   else
     {
-      for(ii = 0; ii <= (ev_header[0]); ii++)
+      for(ii = 0; ii <= (bank_header[0]); ii++)
 	u32p[ii] = bswap_32(u32p[ii]);
     }
 #endif /* BYTESWAPIT */
 
 
-  return (ev_header[0] + 1);
+  return (bank_header[0] + 1);
+
+}
+
+int
+rocBuffer2Bank(const char *inbuf, uint8_t *buf,
+	      uint16_t banktag, uint8_t banknum,
+	      int32_t nbytes)
+{
+
+  int ii, c, ilen, ibyte = 0, rem;
+  int maxb = 1024 * 1024 * 4;	/* max output buffer size */
+  uint32_t bank_header[2];
+
+  if(nbytes > maxb)
+    nbytes = maxb;		/* Default to 4 MB */
+
+  /* Read in the file to the buffer */
+  ilen = 8;			/* leave the header words */
+  ibyte = 0;
+  while(ibyte < nbytes)
+    {
+      buf[ilen++] = inbuf[ibyte++];
+    }
+  printf("%s: INFO: Read %d bytes into buffer\n", __func__, ilen);
+
+  /* Make sure we null terminate the buffer and pad it out to an
+     integal number of words */
+  rem = 4 - (ilen & 0x3);
+  if(rem < 4)
+    {
+      for(ii = 0; ii < rem; ii++)
+	buf[(ilen + ii)] = 0;
+      ilen += ii;
+    }
+  else
+    {
+      rem = 0;			/* no padding necessary just NULL terminate */
+      buf[ilen] = 0;
+    }
+
+  /* Write the header info in the buffer */
+  /* Bank Tag */
+  bank_header[0] = (ilen>>2) - 1; /* divide by 4  and subtract 1 */
+  bank_header[1] = (banktag<<16)|(rem<<14)|(3<<8)| banknum;
+  memcpy(&buf[0],&bank_header[0],8);
+
+  /*Swap the bytes going to the Async Event 32 bit Fifo */
+#ifdef BYTESWAPIT
+  if(0)
+    {
+      uint8_t aa, bb;
+      for(ii = 8; ii <= (bank_header[0] << 2); ii += 4)
+	{
+	  aa = buf[ii];
+	  bb = buf[ii + 1];
+	  buf[ii] = buf[ii + 3];
+	  buf[ii + 1] = buf[ii + 2];
+	  buf[ii + 2] = bb;
+	  buf[ii + 3] = aa;
+	}
+    }
+  else
+    {
+      for(ii = 0; ii <= (bank_header[0]); ii++)
+	u32p[ii] = bswap_32(u32p[ii]);
+    }
+#endif /* BYTESWAPIT */
+
+
+  return (bank_header[0] + 1);
 
 }
